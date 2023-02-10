7cd4bbf9fac1b5918884a03b816f3740c43a8bac
Tommyscripts <dark.angel.alhue@gmail.com>
2023-02-10 10:35:17 +0000

    back end is done

---
+++ api/models/users.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  surname: {
+    type: String,
+    required: [true, 'Surname is required']
+  },
+  birthDate:{
+    type: Date,
+  },
+  identification:{
+    type: String,
+    required: [true, 'ID is required']
+  },
+  reservation:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'reservations',
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    validate: {
+      validator (value) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value)
+      }
+    },
+    unique: [true, 'This is email is registered']
+  },
+  phone: {
+    type: Number,
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'client'],
+    default: 'client'
+  }
+})
+
+const userModel = mongoose.model('user', userSchema)
+module.exports = userModel
